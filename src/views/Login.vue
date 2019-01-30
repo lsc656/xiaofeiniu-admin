a82ada6447460ab8edca533619f3474d0f111bc5
lsc656 <854332104@qq.com>
2019-01-30 16:23:43 +0800

    sdwdqw

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="login">
-    <el-card>
+    <el-card class="xfn-login-card">
       <div slot="header">管理员登录</div>
       <div>
-        
+        <el-form label-width="100px">
+          <el-form-item label="管理员名：">
+            <el-input v-model="formData.aname" placeholder="请输入管理员名"></el-input>
+          </el-form-item>
+          <el-form-item label="登录密码：">
+            <el-input v-model="formData.apwd" type="password" placeholder="请输入管理员密码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="doLogin">登录</el-button>
+            <el-button @click="doCancel">取消</el-button>
+          </el-form-item>
+        </el-form>
       </div>
     </el-card>
   </div>
@@ -13,11 +24,37 @@
   export default {
     data(){
       return {
-
+        formData:{
+          aname:'',
+          apwd:''
+        }
       }
     },
+    methods:{
+      doLogin(){
+        //执行登录
+        var apiUrl = this.$store.state.globalSettings.apiUrl
+        var url = apiUrl+"admin/login/"+this.formData.aname+"/"+this.formData.apwd
+        this.$axios.get(url).then((res)=>{
+          console.log(res)
+        })
+      },
+      doCancel(){
+        //清除用户输入
+        this.formData.aname = ''
+        this.formData.apwd = ''
+      }
+    }
   }
 </script>
 <style lang="scss">
-  
+  .xfn-login-card{
+    width:400px;
+    margin:100px auto;
+    
+    .el-card__header{
+      text-align:center;
+      font-size:1.2em;
+    }
+  }
 </style>
